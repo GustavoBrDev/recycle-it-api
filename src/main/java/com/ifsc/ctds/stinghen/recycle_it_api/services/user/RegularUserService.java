6602a8d1035d85c89eb793767973d7143e9a0300
Gustavo Stinghen <gustavo.s041@aluno.ifsc.edu.br>
2026-02-21 16:43:50 -0300

    Construção da Service
    Prosseguindo com a construção da service de usuário com algumas modificações vistas necessárias

--- src/main/java/com/ifsc/ctds/stinghen/recycle_it_api/services/user/RegularUserService.java
+++ src/main/java/com/ifsc/ctds/stinghen/recycle_it_api/services/user/RegularUserService.java
@@ -1,17 +1,24 @@
 package com.ifsc.ctds.stinghen.recycle_it_api.services.user;
 
+import com.ifsc.ctds.stinghen.recycle_it_api.dtos.request.user.RegularUserPutRequestDTO;
 import com.ifsc.ctds.stinghen.recycle_it_api.dtos.request.user.RegularUserRequestDTO;
 import com.ifsc.ctds.stinghen.recycle_it_api.dtos.response.FeedbackResponseDTO;
 import com.ifsc.ctds.stinghen.recycle_it_api.dtos.response.ResponseDTO;
+import com.ifsc.ctds.stinghen.recycle_it_api.dtos.response.user.FullUserResponseDTO;
+import com.ifsc.ctds.stinghen.recycle_it_api.dtos.response.user.SimpleUserResponseDTO;
+import com.ifsc.ctds.stinghen.recycle_it_api.exceptions.NotFoundException;
 import com.ifsc.ctds.stinghen.recycle_it_api.models.user.RegularUser;
+import com.ifsc.ctds.stinghen.recycle_it_api.models.user.User;
 import com.ifsc.ctds.stinghen.recycle_it_api.repository.user.RegularUserRepository;
 import com.ifsc.ctds.stinghen.recycle_it_api.security.repository.UserCredentialsRepository;
-import jakarta.transaction.Transactional;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Service para os métodos específicos do usuário regular
+ * Service para os métodos específicos do usuário padrão (regular)
  */
 @AllArgsConstructor
 @Service
@@ -19,7 +26,13 @@ public class RegularUserService {
 
     public RegularUserRepository repository;
     public UserCredentialsRepository credentialsRepository;
+    public PasswordEncoder passwordEncoder;
 
+    /**
+     * Cria/persiste o registro de um usuário no banco de dados
+     * @param requestDTO DTO de request de usuários
+     * @return uma {@link ResponseDTO} do tipo {@link FeedbackResponseDTO} informando o status da operação
+     */
     @Transactional
     public ResponseDTO create (RegularUserRequestDTO requestDTO ){
         RegularUser user = requestDTO.convert();
@@ -33,6 +46,7 @@ public class RegularUserService {
                     .build();
         }
 
+        user.getCredential().setPassword( passwordEncoder.encode(user.getCredential().getPassword()));
         repository.save(user);
 
         return FeedbackResponseDTO.builder()
@@ -43,7 +57,120 @@ public class RegularUserService {
     }
 
     @Transactional
+    public ResponseDTO update(Long id, RegularUserPutRequestDTO requestDTO) {
+
+        RegularUser user = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Usuário não encontrado com id: " + id
+                ));
+
+        user.setName(requestDTO.name);
+        user.setCurrentAvatar(requestDTO.avatar);
+        user.getCredential().setPassword(requestDTO.password);
+
+        repository.save(user);
+
+        return FeedbackResponseDTO.builder()
+                .mainMessage("Usuário atualizado com sucesso")
+                .isAlert(false)
+                .isError(false)
+                .build();
+    }
+
+    /**
+     * Obtem o objeto de Usuário pelo id fornecido
+     * @param id o id a ser buscado
+     * @return o usuário em forma de {@link RegularUser}
+     */
+    @Transactional (readOnly = true)
     public RegularUser getObjectById ( Long id ){
         return repository.findById(id).get();
     }
+
+    /**
+     * Obtém a response completa de um usuário pelo id fornecido
+     * @param id o id a ser buscado
+     * @return o usuário em forma da DTO {@link FullUserResponseDTO}
+     * @throws NotFoundException quando o usuário não for encontrado
+     */
+    @Transactional (readOnly = true)
+    public ResponseDTO getFullById ( Long id ){
+
+        if ( repository.existsById(id) ){
+            return new FullUserResponseDTO(repository.findById(id).get());
+        }
+
+        throw new NotFoundException("Usuário não encontrado com o ID " + id );
+    }
+
+    /**
+     * Obtém a response breve de um usuário pelo id fornecido
+     * @param id o id a ser buscado
+     * @return o usuário em forma da DTO {@link SimpleUserResponseDTO}
+     * @throws NotFoundException quando o usuário não for encontrado
+     */
+    @Transactional (readOnly = true)
+    public ResponseDTO getSimpleById ( Long id ){
+
+        if ( repository.existsById(id) ){
+            return new SimpleUserResponseDTO(repository.findById(id).get());
+        }
+
+        throw new NotFoundException("Usuário não encontrado com o ID " + id );
+    }
+
+    /**
+     * Obtem o objeto de Usuário pelo e-mail fornecido
+     * @param email o e-mail a ser buscado
+     * @return o usuário em forma de {@link RegularUser}
+     */
+    @Transactional (readOnly = true)
+    public RegularUser getObjectByEmail ( String email ){
+        return (RegularUser) credentialsRepository.findByEmail(email).get().getUser();
+    }
+
+    /**
+     * Obtém a response completa de um usuário pelo e-mail fornecido
+     * @param email o e-mail a ser buscado
+     * @return o usuário em forma da DTO {@link FullUserResponseDTO}
+     * @throws NotFoundException quando o usuário não for encontrado
+     */
+    @Transactional (readOnly = true)
+    public ResponseDTO getFullByEmail ( String email ){
+
+        if ( credentialsRepository.existsByEmail(email) ){
+            User user = credentialsRepository.findByEmail(email).get().getUser();
+
+            if ( user instanceof RegularUser ){
+                return new FullUserResponseDTO( (RegularUser) user);
+            }
+
+        }
+
+        throw new NotFoundException("Usuário não encontrado com o e-mail " + email );
+    }
+
+    /**
+     * Obtém a response breve de um usuário pelo e-mail fornecido
+     * @param email o e-mail a ser buscado
+     * @return o usuário em forma da DTO {@link SimpleUserResponseDTO}
+     * @throws NotFoundException quando o usuário não for encontrado
+     */
+    @Transactional (readOnly = true)
+    public ResponseDTO getSimpleByEmail ( String email ){
+
+        if ( credentialsRepository.existsByEmail(email) ){
+            User user = credentialsRepository.findByEmail(email).get().getUser();
+
+            if ( user instanceof RegularUser ){
+                return new SimpleUserResponseDTO( (RegularUser) user);
+            }
+
+        }
+
+        throw new NotFoundException("Usuário não encontrado com o e-mail " + email );
+    }
+
+
+
 }
